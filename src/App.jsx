d1e94d99e38db264262dbdbc4b8910d99f29d9f7
harfool gujjar <110320932+harfool@users.noreply.github.com>
2024-12-26 13:34:07 +0530

    Add files via upload
    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './app.scss'
+import Navbar from './components/navbar/Navbar';
+const App = () => {
+  return <div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>parellex</section>
+    <section>services</section>
+    <section>parellex</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section>portfolio3</section>
+    <section>contact</section>
+  </div>;
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion'
+import './navbar.scss'
+import Sidebar from '../sidebar/Sidebar'
+ const Navbar= ()=>  {
+  return (
+    <div className='navbar'>
+      <Sidebar/>
+        <div className="wraper">
+            <motion.span
+             initial={{opacity :0 , scale :0.5}}
+             animate={{opacity :1 , scale :1}}
+             transition={{duration: 0.5}}
+             >harfool gujjar
+             </motion.span>
+            <div className="socialLink">
+               <a href=""><img src="/public/linkdin.png" alt="" /></a>
+               <a href=""><img src="/public/instagram1.png" alt="" /></a>
+               <a href=""><img src="/public/github.png" alt="" /></a>
+               <a href=""><img src="/public/whatsapp.png" alt="" /></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
